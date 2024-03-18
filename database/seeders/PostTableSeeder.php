107aec702894049fdd25efd69e76af6ecfb47414
Minurte1 <hohoangphucjob@gmail.com>
2024-03-18 19:55:42 +0700

    fake success databse

--- database/seeders/PostTableSeeder.php
+++ database/seeders/PostTableSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory;
+use Faker\Generator; // Thêm dòng này để nhập lớp Faker\Generator
+use App\Models\Post;
 
 class PostTableSeeder extends Seeder
 {
@@ -15,5 +18,12 @@ class PostTableSeeder extends Seeder
     public function run()
     {
         //
+        $faker = Factory::create(); // Thay 'Faker' bằng 'Factory'
+        for ($i = 0; $i <= 50; $i++) {
+            Post::create([
+                'title' => $faker->sentence(6, true),
+                'body' => $faker->paragraphs(3, true)
+            ]);
+        }
     }
 }
