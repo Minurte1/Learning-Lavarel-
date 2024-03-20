b6d46b8c28e2a2e3a85e71b479ca1dccec848898
Minurte1 <hohoangphucjob@gmail.com>
2024-03-20 21:47:22 +0700

    xong buoi 2 nhưng mà chỉ biết haizz =))

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
+
 {
+    protected $table = 'posts';
     use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
