8109f5bbd40c4e3b15ae14aa9c9a569812f398dc
Minurte1 <hohoangphucjob@gmail.com>
2024-03-21 18:25:08 +0700

    bugggg hoaiii

---
+++ database/migrations/2024_03_21_103032_create_baiviet_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Termwind\Components\Raw;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('baiviet', function (Blueprint $table) {
+            $table->increments('ma_bviet');
+            $table->string('tieude', 200);
+            $table->string('ten_bhat', 100);
+            $table->text('tomtat');
+            $table->text('noidung')->nullable();
+
+            $table->dateTime('ngayviet')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->string('hinhanh', 200)->nullable();
+
+            $table->unsignedInteger('ma_tgia');
+            $table->unsignedInteger('ma_tloai');
+
+            $table->foreign('ma_tloai')->references('ma_tloai')->on('theloai')->onDelete('cascade');
+            $table->foreign('ma_tgia')->references('ma_tgia')->on('tacgia')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('baiviet');
+    }
+};
