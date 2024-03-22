8bc95b8174e3218471868934bda32269152fc536
Minurte1 <hohoangphucjob@gmail.com>
2024-03-22 21:05:07 +0700

    cap nhat

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,11 @@
+@section('title')
+Home
+@endsection
 @extends('layouts.app')
-@extends('layouts.header')
+
+{{-- @extends('layouts.header') --}}
 @section('content')
   <h1>Trang chủ</h1>
   <p>Đây là nội dung trang chủ.</p>
 @endsection
+
